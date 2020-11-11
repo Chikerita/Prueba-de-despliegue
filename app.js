b8070ba55a754ee2af2b271bd4a9ccf3d307c235
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-11-10 20:20:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,16 +5,37 @@ const PORT = process.env.PORT || 3000;
 
 appServer.use(express.json());
 
-var MongoClient = mongo.MongoClient;
-var url = "mongodb://localhost:27017";
+var url = "mongodb+srv://admin:Str4ngeWorld.@pruebadedespliegue.uwb3h.mongodb.net/mydb?retryWrites=true&w=majority";
+var MongoClient = mongo.MongoClient(url);
 
 appServer.listen(PORT, () => {
     console.log("SERVER IS LISTEN ON PORT:" , PORT);
 });
 
 appServer.get('/get', (req, res) => {
-    res.send("Funciona");
+
+    await listDatabases();
+
+    MongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db("mydb");
+        const options = {
+            sort: {},
+            projection: { _id: 0},
+        };
+        db.collection('CuestionariosPublicos').find({}, options).toArray(function (err, result){
+            if (err) throw err;
+            res.send(result);
+            client.close();
+        });
+    });
 });
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
 
 appServer.put('/put', (req, res) => {
     MongoClient.connect(url, function (err, client) {
