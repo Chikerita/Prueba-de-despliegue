c22736a30e8d393dc88887a6a7d429b9c5972262
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-11-10 22:02:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,37 +5,43 @@ const PORT = process.env.PORT || 3000;
 
 appServer.use(express.json());
 
-var url = "mongodb+srv://admin:Str4ngeWorld.@pruebadedespliegue.uwb3h.mongodb.net/mydb?retryWrites=true&w=majority";
-var MongoClient = mongo.MongoClient(url);
+var url = "mongodb+srv://admin:btCafd9cdd1d29fY@pruebadedespliegue.uwb3h.mongodb.net/mydb?retryWrites=true&w=majority";
+var MongoClient = new mongo.MongoClient(url);
 
 appServer.listen(PORT, () => {
     console.log("SERVER IS LISTEN ON PORT:" , PORT);
 });
 
-appServer.get('/get', (req, res) => {
+appServer.get('/get', async (req, res) => {
+    const results = await findQuestionarys(MongoClient);
+    res.send(results[0].content);
+});
 
-    await listDatabases();
-
-    MongoClient.connect(url, function (err, client) {
-        if (err) throw err;
-        var db = client.db("mydb");
+async function findQuestionarys(client) {
+    try {
+        await client.connect();
         const options = {
             sort: {},
             projection: { _id: 0},
         };
-        db.collection('CuestionariosPublicos').find({}, options).toArray(function (err, result){
-            if (err) throw err;
-            res.send(result);
-            client.close();
-        });
-    });
-});
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+        const cursor = client.db("mydb").collection('CuestionariosPublicos').find({}, options);
+        const results = await cursor.toArray();
+        console.log(results[0].content);
+        if (results.length > 0) {
+            console.log('Questionarys: ');
+            results.forEach((result, i) => {
+                console.log(`${result.content}`);
+            });
+        } else {
+            console.log('No found');
+        }
+        return results;
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
 
 appServer.put('/put', (req, res) => {
     MongoClient.connect(url, function (err, client) {
